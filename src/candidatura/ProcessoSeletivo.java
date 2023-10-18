f7a6565882446a3389120ca3baa0dc5322e8ef86
Thiago Marques <thiagossmarques@gmail.com>
2023-10-18 09:11:37 -0300

    chore: adiciona método para entrar em contato com candidato

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,41 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "MONICA", "EDUARDO"};
 
-        selecaoCandidatos();
+        for(String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            //Alterações
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        } while(continuarTentando && tentativasRealizadas <3); 
+
+        if(atendeu) {
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "TENTATIVA");
+        } else {
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM "+ candidato + ", NÚMERO MÁXIMO TENTATIVAS REALIZADAS = "+ tentativasRealizadas);
+        }
+
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void selecaoCandidatos() {
